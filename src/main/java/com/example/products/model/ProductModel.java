17877acd965372dfe4e95790a1b64954135af535
luckraw <krawdev@gmail.com>
2023-06-18 22:20:22 -0300

    controller, model, repository

---
+++ src/main/java/com/example/products/model/ProductModel.java
@@ -0,0 +1,43 @@
+package com.example.products.model;
+
+import jakarta.persistence.*;
+import java.util.UUID;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tb_products")
+public class ProductModel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private static final long serialVersionUID = 1L;
+    private UUID idProduct;
+    private String name;
+    private BigDecimal value;
+
+    public UUID getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(UUID idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+}
