edc82966804b537f0adaaedf18e76beefb6db9e7
luckraw <krawdev@gmail.com>
2023-06-19 01:47:57 -0300

    hateoas, lombok

--- src/main/java/com/example/products/model/ProductModel.java
+++ src/main/java/com/example/products/model/ProductModel.java
@@ -1,13 +1,16 @@
 package com.example.products.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.hateoas.RepresentationModel;
-
-import java.util.UUID;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.UUID;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "tb_products")
 public class ProductModel extends RepresentationModel<ProductModel> implements Serializable {
@@ -18,29 +21,7 @@ public class ProductModel extends RepresentationModel<ProductModel> implements S
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID idProduct;
     private String name;
+
     private BigDecimal value;
 
-    public UUID getIdProduct() {
-        return idProduct;
-    }
-
-    public void setIdProduct(UUID idProduct) {
-        this.idProduct = idProduct;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public BigDecimal getValue() {
-        return value;
-    }
-
-    public void setValue(BigDecimal value) {
-        this.value = value;
-    }
 }
